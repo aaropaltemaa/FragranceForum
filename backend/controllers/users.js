a93cb9be9c1ccde1bb2a1027db2d06c131492a87
Aaro Paltemaa <apaltemaa@gmail.com>
2024-07-11 16:21:11 +0300

    removed bcrypt

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,5 @@
 const User = require('../models/user')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const usersRouter = require('express').Router()
 
 usersRouter.post('/', async (request, response) => {
