547d98a76227a631d03eca1a5da279dc38725b2d
Aaro Paltemaa <apaltemaa@gmail.com>
2024-07-11 20:41:38 +0300

    new build

--- frontend/components/Display_Reviews/FragranceCard.jsx
+++ frontend/components/Display_Reviews/FragranceCard.jsx
@@ -57,7 +57,7 @@ ShowReviews.propTypes = {
 const FragranceCard = ({ fragrance }) => {
     const dispatch = useDispatch();
     const user = useSelector(state => state.login.user)
-    const staticFilesBaseUrl = import.meta.env.REACT_APP_STATIC_FILES_BASE_URL || 'http://localhost:3001';
+    const staticFilesBaseUrl = import.meta.env.REACT_APP_STATIC_FILES_BASE_URL
 
     const handleDelete = (id) => {
         dispatch(removeFragrance(id));
